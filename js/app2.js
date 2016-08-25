dbe4ee4553c93747c167cd3d2ab04f8340a6c4c6
YuiKyoko <kimberlyesquivelgala@gmail.com>
2016-08-25 15:47:21 -0500

    ejercicio2

---
+++ js/app2.js
@@ -0,0 +1,6 @@
+var num1 = parseInt (prompt("ingresa un numero"))
+var num2 = parseInt (prompt("ingresa un numero"))
+var num3 = parseInt (prompt("ingresa un numero"))
+var resultado =(num1+num2+num3) /3;
+document.write(resultado);
+
